2a3b908253a23b0441b174b055417cfdfdccf07f
telpvn <telpvn@mail.ru>
2025-01-09 16:16:42 +0400

    Scanner

---
+++ untitled1/src/main/java/MyApp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int n = scanner.nextInt();
+        System.out.println("Вы ввели число " + n);
+    }
+}
