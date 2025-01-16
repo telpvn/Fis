a60d31fbece1c64ba907827f23eb233f599b715c
telpvn <telpvn@mail.ru>
2025-01-16 18:59:52 +0400

    Variables and data types complex version

---
+++ untitled2/src/main/java/MainApp.java
@@ -0,0 +1,12 @@
+public class MainApp {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println(age);
+        float f = 10.0f;
+        System.out.println(f);
+        String str = "Hello";
+        System.out.println(str);
+        char ch = '(';
+        System.out.println(ch);
+    }
+}
