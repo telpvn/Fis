1d14b98397c54a3d5289aceb021bf055abdf56a3
telpvn <telpvn@mail.ru>
2025-02-26 09:29:38 +0400

    Arithmetic operations

---
+++ untitled4/src/main/java/MainApp.java
@@ -0,0 +1,12 @@
+public class MainApp {
+    public static void main(String[] args) {
+        double a = 10.0;
+//        a += 5;
+//        a -= 4;
+//        a *= 3;
+//        a /= 2;
+//        a++;
+        a /= 3.0;
+        System.out.println(a);
+    }
+}
